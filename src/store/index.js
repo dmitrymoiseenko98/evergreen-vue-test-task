aa4359f31f28257a89d39bcde479aa1cd5cfe893
Dmitry Moiseenko <dmitrymoiseenko@outlook.com>
2020-04-07 13:46:17 +0300

    Get all users

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,22 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    users: [],
+  },
+  getters: {
+    users: (state) => state.users,
   },
   mutations: {
+    setUsers(state, users) {
+      state.users = users;
+    },
   },
   actions: {
-  },
-  modules: {
+    async getUsers(context) {
+      const users = await fetch('https://reqres.in/api/users?page=1')
+        .then((res) => res.json());
+
+      context.commit('setUsers', users.data);
+    },
   },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,55 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="users">
+      <div class="user" v-for="user in users" :key="user.id">
+        <img class="user__avatar" :src="user.avatar" alt="avatar">
+        <p class="user__name">{{user.first_name}} {{user.last_name}}</p>
+        <p class="user__email">{{user.email}}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
   name: 'Home',
+  computed: mapGetters(['users']),
+  methods: mapActions(['getUsers']),
+  mounted() {
+    this.getUsers();
+  },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.users {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 960px;
+  margin: auto;
+}
+
+.user {
+  width: 300px;
+  margin: 10px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+
+.user__avatar {
+  border-radius: 100%;
+}
+
+.user__name,
+.user__email {
+  margin: 15px 0 0;
+}
+</style>
