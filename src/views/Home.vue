a820da90844ed820350a0fcdfc1c184c91160d34
Dmitry Moiseenko <dmitrymoiseenko@outlook.com>
2020-04-11 16:23:34 +0300

    User pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,53 @@
           <label>Search by name:</label>
           <input type="text" v-model="searchName">
         </div>
+        <div class="users-filter__item users-filter__amount-per-page">
+          <label>Display per page:</label>
+          <select v-model="usersPerPage">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+          </select>
+        </div>
       </form>
       <ul class="users-list">
-        <li class="user" v-for="user in filteredUsers" :key="user.id">
+        <li class="user" v-for="user in userPages[page - 1]" :key="user.id">
           <img class="user__avatar" :src="user.avatar" alt="avatar">
           <p class="user__name">{{user.first_name}} {{user.last_name}}</p>
           <p class="user__email">{{user.email}}</p>
         </li>
       </ul>
+      <div
+        class="users-pagination"
+        v-if="userPages.length > 1"
+      >
+        <button
+          class="users-pagination__prev users-pagination__btn"
+          :disabled="page === 1"
+          @click="goToPage(page - 1)"
+        >
+          <span>Prev</span>
+        </button>
+        <div class="users-pagination__pages">
+          <button
+            type="button"
+            class="users-pagination__pages__page users-pagination__btn"
+            :key="index"
+            :disabled="page === index + 1"
+            @click="goToPage(index + 1)"
+            v-for="(pageUsers, index) in userPages"
+          >
+            <span>{{index + 1}}</span>
+          </button>
+        </div>
+        <button
+          class="users-pagination__next users-pagination__btn"
+          :disabled="page === userPages.length"
+          @click="goToPage(page + 1)"
+        >
+          <span>Next</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,6 +65,8 @@ export default {
   data() {
     return {
       searchName: '',
+      usersPerPage: 5,
+      page: 1,
     };
   },
   computed: {
@@ -34,8 +75,22 @@ export default {
       return this.users.filter((user) => user.full_name.toLowerCase()
         .includes(this.searchName));
     },
+    userPages() {
+      let res = [];
+
+      for (let from = 0; from < this.filteredUsers.length; from += this.usersPerPage) {
+        res = [...res, this.filteredUsers.slice(from, from + this.usersPerPage)];
+      }
+
+      return res;
+    },
   },
-  methods: mapActions(['getUsers']),
+  methods: {
+    ...mapActions(['getUsers']),
+    goToPage(page) {
+      this.page = page;
+    },
+  },
   mounted() {
     this.getUsers();
   },
@@ -59,6 +114,16 @@ body {
   max-width: 1000px;
   margin: auto;
   padding: 0 20px;
+}
+
+.users-filter {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.users-filter__item {
+  margin: 10px 20px;
 }
 
 .users-filter__item label {
@@ -89,5 +154,51 @@ body {
 .user__name,
 .user__email {
   margin: 15px 0 0;
+}
+
+.users-pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
+
+.users-pagination__btn {
+  background-color: transparent;
+  border: 1px solid #ccc;
+  padding: 12px;
+  cursor: pointer;
+  outline: none;
+}
+
+.users-pagination__btn:not(:disabled):hover {
+  background-color: rgba(0, 0, 0, .05);
+}
+
+.users-pagination__btn:not(:disabled):active {
+  background-color: rgba(0, 0, 0, .1);
+}
+
+.users-pagination__btn:disabled {
+  cursor: default;
+}
+
+.users-pagination__btn:disabled span {
+  opacity: .5;
+}
+
+.users-pagination__pages__page {
+  border-left: none;
+}
+
+.users-pagination__pages__page:last-child {
+  border-right: none;
+}
+
+.users-pagination__prev {
+  border-radius: 10px 0 0 10px;
+}
+
+.users-pagination__next {
+  border-radius: 0 10px 10px 0;
 }
 </style>
